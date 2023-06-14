9c15c01c3bd1d733d8eac98d883d42b674c94d28
Erika <erika.s.yamamoto@gmail.com>
2023-06-14 15:38:31 -0300

    reeq 1, 2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
-};
+const { species } = data;
+const getSpeciesByIds = (...ids) => species
+  .filter((animals) => ids.includes(animals.id));
 
 module.exports = getSpeciesByIds;
