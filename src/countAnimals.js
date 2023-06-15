352e3abaef604d9941e1f56b9e0dc2b54b817060
Erika <erika.s.yamamoto@gmail.com>
2023-06-14 21:33:44 -0300

    req5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,24 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => data.popularity
-  .filter((popularitySpecies))
+const countAnimals = (animal) => {
+  if (!animal) {
+    const animalCounts = {};
+    data.species.forEach((specie) => {
+      animalCounts[specie.name] = specie.residents.length;
+    });
+    return animalCounts;
+  }
+
+  if (!animal.sex) {
+    return data.species.find((specie) => specie.name === animal.species)
+      .residents.length;
+  }
+
+  const countSexFilter = data.species
+    .find((specie) => specie.name === animal.species)
+    .residents.filter((resident) => resident.sex === animal.sex);
+
+  return countSexFilter.length;
+};
 
 module.exports = countAnimals;
