469f0f26beab5eaf2ea29877222f09add9ffbbb6
Erika <erika.s.yamamoto@gmail.com>
2023-06-16 20:32:06 -0300

    req10 e lint ok

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const responsibleFirst = data.employees
+    .find((employee) => employee.id === id).responsibleFor[0];
+
+  const animal = data.species
+    .find((specie) => specie.id === responsibleFirst).residents;
+  return Object.values(animal.sort((a, b) => a.age - b.age)[animal.length - 1]);
 };
 
 module.exports = getOldestFromFirstSpecies;
